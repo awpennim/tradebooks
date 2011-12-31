079f0bc00a71abeef94a50c6c39803eb56d48f3f
Andrew Penniman <awpennim@student.umass.edu>
2011-12-30 20:41:42 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zoomasstextbooks::Application.config.secret_token = '97ad21010ed42e25e501a20cd4a4149a5b98c4c6197afd9d7969c61f5a49671733418a2b7f2765a53b0142773a95b6666f55136efe094ba019194e58946e049e'
