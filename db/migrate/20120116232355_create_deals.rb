5039b49a33508a8abd017abae00937ea53e63cb0
Andrew Penniman <awpennim@student.umass.edu>
2012-01-17 00:07:36 -0500

    message

--- db/migrate/20120116232355_create_deals.rb
+++ db/migrate/20120116232355_create_deals.rb
@@ -1,6 +1,6 @@
 class CreateDeals < ActiveRecord::Migration
   def self.up
-    create_table :deals do |t|
+    create_table :completed_deals do |t|
       t.integer :buyer_id
       t.integer :seller_id
       t.integer :seller_status, :default => 0
@@ -9,11 +9,11 @@ class CreateDeals < ActiveRecord::Migration
       t.timestamps
     end
 
-    add_index :deals, :buyer_id
-    add_index :deals, :seller_id
+    add_index :completed_deals, :buyer_id
+    add_index :completed_deals, :seller_id
   end
 
   def self.down
-    drop_table :deals
+    drop_table :completed_deals
   end
 end
