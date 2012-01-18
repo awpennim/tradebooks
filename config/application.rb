cc333c57fcb395efef26fe7ce2d7a0727a4c1c78
Andrew Penniman <awpennim@student.umass.edu>
2012-01-18 00:24:21 -0500

    mailer working

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module Zoomasstextbooks
   class Application < Rails::Application
+    config.action_mailer.default_url_options = {:host => 'tradecampusbooks.com'}
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
