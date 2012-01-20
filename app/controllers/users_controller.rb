7b05e4da31bc08c0dbb03ac242406f5528ef2a92
Andrew Penniman <awpennim@student.umass.edu>
2012-01-19 22:27:07 -0500

    users order

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
 
 
   def index
-    @users = User.paginate(:page => params[:page])
+    @users = User.paginate(:page => params[:page], :order => 'created_at DESC')
     @title = "All Users"
   end
 
