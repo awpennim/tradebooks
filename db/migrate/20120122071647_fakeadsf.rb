3893aa5db20b9ad7d522635c00d5e62e64a9cedb
Andrew Penniman <awpennim@student.umass.edu>
2012-01-22 02:42:00 -0500

    dumb migration

--- db/migrate/20120122071647_fakeadsf.rb
+++
@@ -1,9 +0,0 @@
-class Fakeadsf < ActiveRecord::Migration
-  def self.up
-    change_column :notifications, :messages, :string
-  end
-
-  def self.down
-    change_column :notifications, :messages, :text
-  end
-end
