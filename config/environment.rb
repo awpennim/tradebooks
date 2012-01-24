a54c103dff29c13075c428d6f23992c8a22bcdb5
Andrew Penniman <awpennim@student.umass.edu>
2012-01-24 17:16:07 -0500

    asdfds

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,6 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-ActionMailer::Base
 
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
