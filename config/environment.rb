e0ec6835b7c53c7e62f3be858e0509ec6f99cb53
Andrew Penniman <awpennim@student.umass.edu>
2012-01-24 10:35:48 -0500

    dsfads

--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,12 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+ActionMailer::Base
+
 # Initialize the rails application
 Zoomasstextbooks::Application.initialize!
 
 RCC_PUB = '6Lc4acwSAAAAAMmysjig5psn13XVwaTwHRYsujoe'
 RCC_PRIV = '6Lc4acwSAAAAAL8yBEhrlD4S_uHa6H99kCtj76NI'
 
-ActionMailer::Base
 
